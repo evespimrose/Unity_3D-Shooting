1fa8a89f11488f3fb0396f7122e4e98d7e54ef1a
evespimrose <bigjht34@gmail.com>
2022-04-22 19:31:03 +0900

    캐릭터 추가, 애니메이터 수정, 플레이어 스크립트 생성, 맵 배치

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,30 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     public float speed;
+    bool walkDown;
 
     Vector3 moveVec;
 
-    void Start()
+    Animator animator;
+
+    void Awake()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        walkDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+ 
+        transform.position += moveVec * speed * Time.deltaTime * (walkDown ? 0.3f : 1f);
 
-        transform.position += moveVec * speed * Time.deltaTime;
+
+
+        animator.SetBool("isRun", moveVec != Vector3.zero);
+        animator.SetBool("isWalk", walkDown);
     }
 }
